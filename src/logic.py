c4db77796a2bb17f2520cbeb307eda6a932dc93e
Graham Sela <grahamsela@Grahams-MBP.hitronhub.home>
2022-06-23 14:50:43 -0700

    maybe??

--- src/logic.py
+++ src/logic.py
@@ -134,7 +134,7 @@ def find_food(my_head, possible_moves, foods):
         return 'right'
     if closest_food['y'] > my_head['y'] and 'up' in possible_moves:
         return 'up'
-    if 'left' in possible_moves:
+    if 'left' in possible_moves and closest_food['y'] < my_head['y']:
         return 'left'
     return 'down'
 
